07277dff0c62a594a0da1e11c870769b0e7ed15a
Humen4ik <dmitrog61@gmail.com>
2024-03-15 14:50:27 +0200

    My basic Html and Css pages

---
+++ Tanks/pages/contact-me/connect.php
@@ -0,0 +1,10 @@
+<?php
+    $first_name = $_POST['first-name'];
+    $second_name = $_POST['second-name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Під'єднання до БД
+
+
+?>
